2fd0514fc3bfb783cd695136f5966a119d5e1f90
christianrrl <christianrod@mail.usf.edu>
2015-10-07 23:15:08 -0400

    HW 3.10 Final Version

--- Assignment 3/MaxMinPour.c
+++ Assignment 3/MaxMinPour.c
@@ -1,5 +1,6 @@
 //File: MaxMinPour
 //Author: Christian Rodriguez
+
 /*Instructions:
  Write a program that will implement max-min scheduling by simulating the 
  pouring process. The program should take as command line input the available 
@@ -22,50 +23,89 @@
  https://msdn.microsoft.com/en-us/library/a1y7w461.aspx
  http://cboard.cprogramming.com/c-programming/134232-converting-command-line-char-*argv%5B%5D-int.html
  */
+
 #include <stdio.h>
+#include <stdlib.h>
 
 int main( int argc, char *argv[] )
 {
-    int available = atoi(argv[1]);
-    int requestCount = argc - 2;
+    int requestCount;
     
+    // Handle different argc inputs
+    if( argc == 1 )
+    {
+        printf("Available bandwidth expected.\n");
+        printf("Up to ten bandwidth requests expected.\n");
+        return 0;
+    }
+    else if ( argc == 2 )
+    {
+        printf("Available bandwidth supplied is %s.\n", argv[1]);
+        printf("Up to ten bandwidth requests expected.\n");
+        return 0;
+    }
+    else if ( argc > 12 )
+    {
+        printf("Over ten bandwidth requests supplied.\n");
+        printf("Only first 10 to be used.\n");
+        requestCount = 10;
+    }
+    else
+    {
+        requestCount = argc - 2;
+    }
+    
+    
+    int availableRequest = atoi(argv[1]);   // Convert argv[1] string to int
     int bandwidthRequest[requestCount];
     int bandwidthCurrent[requestCount];
     
+    // Fill in current bandwidth array and requested bandwidth array
     for (int i = 0; i < requestCount; i++)
     {
         bandwidthRequest[i] = atoi(argv[i+2]);
         bandwidthCurrent[i] = 0;
     }
     
-    int temp = 0;
-    while ((available > 0) && (temp < 3))
+    
+    int filledCount = 0;
+    
+    // Loop until either available bandwidth reaches 0 or all requests are filled
+    while ((availableRequest > 0) && (filledCount < requestCount))
     {
-        temp = 0;
-
+        filledCount = 0;
+        
+        // Fill in each bandwidth request appropriately
         for (int i = 0; i < requestCount; i++)
         {
+            // Handle different empty/filled bandwidth request cases
             if (bandwidthCurrent[i] < bandwidthRequest[i])
             {
                 bandwidthCurrent[i] += 1;
-                available--;
+                availableRequest--;
             }
             else
             {
-                temp++;
+                filledCount++;
             }
-            if (available <= 0) {
+            
+            // Handle running out of available bandwidth
+            if (availableRequest <= 0) {
                 break;
             }
         }
     }
     
+    // Print each allocated bandwidth
     printf("Allocated bandwidth = ");
     for (int i = 0; i < requestCount; i++) {
         printf("%d ", bandwidthCurrent[i]);
     }
     
-    printf("\nAvailable bandwidth after allocations = %d", available);
+    // Print remaining bandwidth
+    printf("\nAvailable bandwidth after allocations = %d", availableRequest);
 
     printf("\n");
+    
+    return 0;
 }
